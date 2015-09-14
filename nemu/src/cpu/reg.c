c98334fe872cdf1f85940ba1c94c26068d272f35
tracer-ics2015 <tracer@njuics.org>
2015-09-14 20:29:34 +0800

    >  compile NEMU 141220000 snlia Linux snliaPC 3.13.0-35-generic #62-Ubuntu SMP Fri Aug 15 01:58:42 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  20:29:34 up 11 min,  3 users,  load average: 0.48, 0.56, 0.43 a1df7d6a3f08087dd1e99ea24962ff12e5517542

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -18,7 +18,7 @@ void reg_test() {
 	for(i = R_EAX; i <= R_EDI; i ++) {
 		sample[i] = rand();
 		reg_l(i) = sample[i];
-		printf ("%d", reg_w (i));
+		printf ("%d", 1);
 		assert(reg_w(i) == (sample[i] & 0xffff));
 	}
 
