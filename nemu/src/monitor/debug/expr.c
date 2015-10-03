b970d73399b962a4c12fd4267acf57ac7525659a
snlia <chinsnlia@gmail.com>
2015-10-03 13:48:53 +0800

    add regrister rule in expr.c
    add new rules : regrister(* To avoid conflic with regrister and other operation, add Size_7 to other operation)	modified:   nemu/src/monitor/debug/expr.c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,11 +5,14 @@
  */
 #include <sys/types.h>
 #include <regex.h>
+#define Size_7 128
 #define downcase(x) ((x) <= 'Z' && (x) >= 'A' ? (x) - 'A' + 'a' : (x))
 
 enum {
-	NOTYPE = 256, EQ, UEQ, NOT, NUMBER_D, NUMBER_H, PRE_MUL, PRE_PLUS, PRE_SUBTRACT
-
+	NOTYPE = 256, EQ, UEQ, NOT, NUMBER_D, NUMBER_H, PRE_MUL, PRE_PLUS, PRE_SUBTRACT,
+	EAX, ECX, EDX, EBX, ESP, EBP, ESI, EDI, EIP,
+	AX, CX, DX, BX, SP, BP, SI, DI,
+	AL, CL, DL, BL, AH, CH, DH, BH
 	/* TODO: Add more token types */
 
 };
@@ -22,19 +25,64 @@ static struct rule {
 	/* TODO: Add more rules.
 	 * Pay attention to the precedence level of different rules.
 	 */
-
-	{" +",	NOTYPE},				// spaces
-	{"\\+", '+'},					// plus
-	{"-", '-'},						// subtraction
-	{"\\*", '*'},					// multiplication
-	{"/", '/'},						// division
-	{"0x[0-9abcdef]+", NUMBER_H},	// hexadecimal number
-	{"[0-9]+", NUMBER_D},			// decimal number
-	{"\\(", '('},					// left parentheses
-	{"\\)", ')'},					// right parentheses
-	{"==", EQ},						// equal
-	{"!==", UEQ},					// unequal
-	{"!", NOT}						// equal to zero
+	//(flod) ' ', '+' , '-' , '*' , '/', '&', '|'
+	{" +",	NOTYPE},					// spaces
+	{"\\+", '+' + Size_7},				// plus
+	{"-", '-' + Size_7},				// subtraction
+	{"\\*", '*' + Size_7},				// multiplication
+	{"/", '/' + Size_7},				// division
+	{"&", '&' + Size_7},				// &
+	{"|", '|' + Size_7},				// or
+	{"^", '^' + Size_7},				// xor
+	//(flod end)
+	//(flod) Number (decimal & hexadecimal)
+	{"0x[0-9abcdef]+", NUMBER_H},		// hexadecimal number
+	{"[0-9]+", NUMBER_D},				// decimal number
+	//(flod end)
+	//(flod) Parentheses
+	{"\\(", '(' + Size_7},				// left parentheses
+	{"\\)", ')' + Size_7},				// right parentheses
+	//(flod end)
+	//(flod) Judging expression
+	{"==", EQ},							// equal
+	{"!=", UEQ},						// unequal
+	{"!", NOT}							// equal to zero
+	{"&", NOT}							// equal to zero
+	{"!", NOT}							// equal to zero
+	{"!", NOT}							// equal to zero
+	//(flod) 32 bit Register
+	{"\\$eax", EAX}						//eax
+	{"\\$ecx", ECX}						//ecx
+	{"\\$edx", EDX}						//edx
+	{"\\$ebx", EBX}						//ebx
+	{"\\$esp", ESP}						//esp
+	{"\\$ebp", EBP}						//ebp
+	{"\\$esi", ESI}						//esi
+	{"\\$edi", EDI}						//edi
+	//(flod end)
+	{"\\$eip", EIP}						//eip
+	//(flod) 16 bit Register
+	{"\\$ax", AX}						//ax
+	{"\\$cx", CX}						//cx
+	{"\\$dx", DX}						//dx
+	{"\\$bx", BX}						//bx
+	{"\\$sp", SP}						//sp
+	{"\\$bp", BP}						//bp
+	{"\\$si", SI}						//si
+	{"\\$di", DI}						//di
+	//(flod end)
+	//(flod) 8 bit Register
+	{"\\$al", AL}						//al
+	{"\\$cl", CL}						//cl
+	{"\\$dl", DL}						//dl
+	{"\\$bl", BL}						//bl
+	{"\\$ah", AH}						//ah
+	{"\\$ch", CH}						//ch
+	{"\\$dh", DH}						//dh
+	{"\\$bh", BH}						//bh
+	//(flod end)
+	//Add more register here
+const char *regsb[] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -90,16 +138,16 @@ static bool make_token(char *e) {
 				 * to record the token in the array ``tokens''. For certain 
 				 * types of tokens, some extra actions should be performed.
 				 */
-				if (!nr_token || (tokens[nr_token - 1].type != NUMBER_D && tokens[nr_token - 1].type != NUMBER_H && tokens[nr_token - 1].type != ')' ))
+				if (!nr_token || (tokens[nr_token - 1].type != NUMBER_D && tokens[nr_token - 1].type != NUMBER_H && tokens[nr_token - 1].type != ')' + Size_7 ))
 				switch(rules[i].token_type)
 				{
-					case '+' :
+					case '+' + Size_7 :
 						tokens[nr_token] = PRE_PLUS;
 						break;
-					case '-' :
+					case '-' + Size_7 :
 						tokens[nr_token] = PRE_SUBTRACT;
 						break;
-					case '*' :
+					case '*' + Size_7 :
 						tokens[nr_token] = PRE_MUL;
 						break;
 					//default: panic("please implement me");
@@ -118,6 +166,11 @@ static bool make_token(char *e) {
 	return true; 
 }
 
+bool check_parentheses (int l, int r)
+{
+	if (tokens[l].type != '(' + Size_7 || tokens[r].type != ')' + Size_7) return 0;
+}
+
 int eval (int p, int q, bool *success)
 {
 	int val1, val2, ans, i, len;
@@ -125,12 +178,16 @@ int eval (int p, int q, bool *success)
 	{
 		if(p > q) 
 		{
-			puts ("Bad expression, please check it again.");
+			puts ("Bad expression, please check it.");
 			success = 0;
 			return 0;
 		}
 		else if(p == q) 
 		{ 
+			if (tokens[p].type == EIP) return cpu.eip;
+			if (tokens[p].type >= EAX && tokens[p].type <= EDI) return reg_l (tokens[p].type - EAX);
+			if (tokens[p].type >= EAX && tokens[p].type <= EDI) return reg_l (tokens[p].type - EAX);
+			if (tokens[p].type >= EAX && tokens[p].type <= EDI) return reg_l (tokens[p].type - EAX);
 			switch (tokens[p].type)
 			{
 				case NUMBER_D :
@@ -140,7 +197,7 @@ int eval (int p, int q, bool *success)
 					len = strlen (tokens[p].str);
 					for (i = 0; i < len; ++i) tokens[p].str[i] = downcase (tokens[p].str[i]);
 					sscanf (tokens[p].str, "%x", &ans);
-
+					//Adress and Register here
 				default : puts ("Bad expression, you may miss a number.");
 			}
 			/* Single token.
@@ -157,28 +214,51 @@ int eval (int p, int q, bool *success)
 		}
 		else 
 		{
-			op = the position of dominant operator in the token expression;
-			val1 = eval(p, op - 1, success);
+			int op = get_dominant (p, q);
+			if (op != p) val1 = eval (p, op - 1, success);
 			if (!success) return 0;
-			val2 = eval(op + 1, q, success);
+			val2 = eval (op + 1, q, success);
 			if (!success) return 0;
 
-			switch(op_type) 
+			switch(tokens[op].type) 
 			{
-				case '+': return val1 + val2;
-				case '-': return val1 + val2;
-				case '*': return val1 * val2;
-				case '/': return val1 / val2;
+				case '+' + Size_7: return val1 + val2;
+				case '-' + Size_7: return val1 + val2;
+				case '*' + Size_7: return val1 * val2;
+				case '/' + Size_7: return val1 / val2;
+				case '&' + Size_7: return val1 & val2;
+				case '|' + Size_7: return val1 | val2;
+				case '^' + Size_7: return val1 ^ val2;
+				case PRE_PLUS : return val2;
+				case PRE_SUBTRACT : return -val2;
+				case PRE_MUL : return swaddr_read(val2, 4);
 				default: assert(0);
 			}
 		}
 	}
 }
 
+bool Check_Parentheses ()
+{
+	int i, flag = 0;
+	for (i = 0; i < nr_token; ++i) 
+	{
+		if (tokens[i].type == '(' + Size_7) ++flag;
+		if (tokens[i].type == ')' + Size_7) --flag;
+		if (flag < 0) return false;
+	}
+	return true;
+}
+
 uint32_t expr(char *e, bool *success) {
 	if(!make_token(e)) 
 	{
 		*success = 0;
+		return 0;
+	}
+	if (Check_Parentheses) // check if parentheses matched first
+	{
+		success = 0; 
 		return 0;
 	}
 
