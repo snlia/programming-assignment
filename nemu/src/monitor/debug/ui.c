4b1d18967b163f36bfb0117c7f63724d1286138c
tracer-ics2015 <tracer@njuics.org>
2015-10-04 16:10:49 +0800

    >  compile NEMU 141220000 snlia Linux snliaPC 3.13.0-35-generic #62-Ubuntu SMP Fri Aug 15 01:58:42 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:10:49 up 7 min,  3 users,  load average: 0.69, 0.73, 0.48 ccff2d34a70f5dc9e6ff7f916a86e425fa908a4e

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -150,9 +150,11 @@ static int cmd_x (char *args)
 		puts ("The first argument should be a number.");
 		return 0;
 	}
+	char *Arg = args + strlen(arg) + 1;
+	printf ("%s\n", Arg);
 	bool flag = 1;
-	printf ("%s\n", args);
-	int adress = expr (args + strlen (arg), &flag);
+	int adress = 0;
+//	int adress = expr (args + strlen (arg), &flag);
 	if (!flag) 
 	{
 		puts ("Something wrong with expression, please check it.");
