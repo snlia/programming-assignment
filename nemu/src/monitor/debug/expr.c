b62d0e5af34f63c42886857777e483a09b52525b
tracer-ics2015 <tracer@njuics.org>
2015-10-08 18:21:09 +0800

    >  compile NEMU 141220000 snlia Linux snliaPC 3.13.0-35-generic #62-Ubuntu SMP Fri Aug 15 01:58:42 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  18:21:09 up 43 min,  3 users,  load average: 0.12, 0.14, 0.20 5d31b408fd64767c7a90a5504af35af5b2992e83

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -379,6 +379,9 @@ uint32_t expr(char *e, bool *success) {
 		*success = false; 
 		return 0;
 	}
+	int i;
+	for (i = 0; i < nr_token; ++i) printf ("%d ", Domination[tokens[i].type]);
+	puts ("");
 
 	/* TODO: Insert codes to evaluate the expression. */
 	//	panic("please implement me");
