fa7da1b0196f4601259749981251a18d826e3b6c
Wu Yuncong <14307130244@fudan.edu.cn>
2015-11-17 19:53:52 +0800

    >  compile NEMU 14307130244 snlia Linux snliaPC 3.13.0-35-generic #62-Ubuntu SMP Fri Aug 15 01:58:42 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  19:53:52 up  3:37,  8 users,  load average: 0.59, 0.39, 0.39 37737887af9298ac8839d7e2d41ead975721e0f3

--- nemu/src/cpu/decode/modrm.c
+++ nemu/src/cpu/decode/modrm.c
@@ -98,7 +98,6 @@ int read_ModR_M(swaddr_t eip, Operand *rm, Operand *reg) {
 			case 4: rm->val = reg_l(m.R_M); break;
 			default: assert(0);
 		}
-        printf ("%d\n", rm->val);
 #ifdef DEBUG
 		switch(rm->size) {
 			case 1: sprintf(rm->str, "%%%s", regsb[m.R_M]); break;
