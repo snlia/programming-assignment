f9eeb9c5b7bec9a37c3d7eea4917386e5f78d9eb
Wu Yuncong <14307130244@fudan.edu.cn>
2015-11-20 20:54:01 +0800

    >  compile NEMU 14307130244 snlia Linux snliaPC 3.13.0-35-generic #62-Ubuntu SMP Fri Aug 15 01:58:42 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  20:54:01 up  2:49,  4 users,  load average: 0.52, 0.47, 0.76 7d9f31c3cafee0d98e60497a16644b689b18bf82

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -5,7 +5,7 @@
 static void do_execute () {
 	cpu.esp -= ops_decoded.is_data_size_16 ? 2 : 4;
 	swaddr_write(cpu.esp, ops_decoded.is_data_size_16 ? 2 : 4, cpu.eip);
-	cpu.eip += op_src.val;
+	cpu.eip += op_src->val;
 	/* TODO: Update EFLAGS. */
 //	panic("please implement me");
 
---
+++ nemu/src/cpu/exec/call/call.h
@@ -0,0 +1,10 @@
+#ifndef __CALL_H__
+#define __CALL_H__
+
+make_helper(call_i_b);
+make_helper(call_i_w);
+make_helper(call_i_l);
+
+make_helper(call_i_v);
+
+#endif
