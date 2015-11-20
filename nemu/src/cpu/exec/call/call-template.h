9cbab4762f70111af84480f92f778fe0af4d924b
Wu Yuncong <14307130244@fudan.edu.cn>
2015-11-20 20:26:09 +0800

    >  run 14307130244 snlia Linux snliaPC 3.13.0-35-generic #62-Ubuntu SMP Fri Aug 15 01:58:42 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  20:26:09 up  2:21,  4 users,  load average: 0.30, 0.27, 0.32 c18f38d2205110ed18a8e548dd7b98aa703a2472

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -3,11 +3,11 @@
 #define instr call
 
 static void do_execute () {
-	cpu.esp -= DATA_TYPE;
+	cpu.esp -= ops_decoded.is_data_size_16 ? 2 : 4;
 	swaddr_write(cpu.esp, DATA_BYTE, cpu.eip);
 	cpu.eip += op_src.val;
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+//	panic("please implement me");
 
 	print_asm_template1();
 }
