2a347acf40608e79fee67aa386698954ca8c2176
Wu Yuncong <14307130244@fudan.edu.cn>
2015-11-22 16:50:27 +0800

    >  compile NEMU 14307130244 snlia Linux snliaPC 3.13.0-35-generic #62-Ubuntu SMP Fri Aug 15 01:58:42 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:50:27 up  1:31,  5 users,  load average: 0.30, 0.17, 0.18 b822dd51b1f44348e6983abde540194589633c91

--- nemu/src/cpu/exec/cmp/cmp-template.h
+++ nemu/src/cpu/exec/cmp/cmp-template.h
@@ -8,7 +8,7 @@ static void do_execute() {
 	cpu.CF = ((uint64_t) op_src->val - (uint64_t) op_dest->val) != (result & 0xffffffff);
 	cpu.OF = cpu.CF ^ cpu.SF;
 	cpu.SF = (result >> 31) & 1;
-	cpu.ZF = (result != 0);
+	cpu.ZF = !result;
 	printf ("%d\n", cpu.ZF);
 	cpu.AF = ((op_src->val & 0x7) - (op_dest->val & 0x7)) < 0x8;
 	result = result & 0xff;
