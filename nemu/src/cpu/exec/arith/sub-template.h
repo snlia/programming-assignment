2880e20bb62a3426aab9023ae66fcab579726ed4
Wu Yuncong <14307130244@fudan.edu.cn>
2015-11-23 18:03:14 +0800

    >  compile NEMU 14307130244 snlia Linux snliaPC 3.13.0-35-generic #62-Ubuntu SMP Fri Aug 15 01:58:42 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  18:03:14 up  1:24,  5 users,  load average: 0.53, 0.60, 0.60 9baae0da2e484ff16acf94024e9e61b84877f333

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -22,5 +22,9 @@ static void do_execute () {
 }
 
 make_instr_helper(r_ib)
+make_instr_helper(i2r)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/arith/sub.h
+++ nemu/src/cpu/exec/arith/sub.h
@@ -2,7 +2,29 @@
 #define __SUB_H__
 
 make_helper(sub_r_ib_b);
+make_helper(sub_r_ib_w);
+make_helper(sub_r_ib_l);
+
+make_helper(sub_i2r_b);
+make_helper(sub_i2r_w);
+make_helper(sub_i2r_l);
+
+make_helper(sub_i2rm_b);
+make_helper(sub_i2rm_w);
+make_helper(sub_i2rm_l);
+
+make_helper(sub_r2rm_b);
+make_helper(sub_r2rm_w);
+make_helper(sub_r2rm_l);
+
+make_helper(sub_rm2r_b);
+make_helper(sub_rm2r_w);
+make_helper(sub_rm2r_l);
 
 make_helper(sub_r_ib_v);
+make_helper(sub_i2r_v);
+make_helper(sub_i2rm_v);
+make_helper(sub_r2rm_v);
+make_helper(sub_rm2r_v);
 
 #endif
