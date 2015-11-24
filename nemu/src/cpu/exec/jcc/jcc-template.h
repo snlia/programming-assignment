1b904e819886759040d0d97da0d3c8785d9bc173
Wu Yuncong <14307130244@fudan.edu.cn>
2015-11-24 20:24:17 +0800

    >  compile NEMU 14307130244 snlia Linux snliaPC 3.13.0-35-generic #62-Ubuntu SMP Fri Aug 15 01:58:42 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  20:24:17 up 25 min,  4 users,  load average: 0.59, 0.43, 0.42 73c83bd537553f0e891746a54c1fe00b5e9169ee

--- nemu/src/cpu/exec/jcc/jcc-template.h
+++ nemu/src/cpu/exec/jcc/jcc-template.h
@@ -2,11 +2,48 @@
 
 #define instr jcc
 
+#define CF cpu.CF
+#define VM cpu.VM
+#define RF cpu.RF
+#define NT cpu.NT
+#define IOPL cpu.IOPL
+#define OF cpu.OF
+#define DF cpu.DF
+#define IF cpu.IF
+#define TF cpu.TF
+#define SF cpu.SF
+#define ZF cpu.ZF
+#define AF cpu.AF
+#define PF cpu.PF
+
+
 static void do_execute () {
-	if (cpu.ZF)
+	bool flag = 0;
+	switch (ops_decoded.opcode) 
+	{
+		case 0x70 : flag =  OF; break; 
+		case 0x71 : flag = !OF; break; 
+		case 0x72 : flag = CF; break; 
+		case 0x73 : flag = !CF; break; 
+		case 0x74 : flag = ZF; break; 
+		case 0x75 : flag = !ZF; break; 
+		case 0x76 : flag = CF || ZF; break; 
+		case 0x77 : flag = !CF && !ZF; break; 
+		case 0x78 : flag =  SF; break; 
+		case 0x79 : flag = !SF; break; 
+		case 0x7a : flag =  PF; break; 
+		case 0x7b : flag = !PF; break; 
+		case 0x7c : flag = SF != OF; break; 
+		case 0x7d : flag = SF == OF; break; 
+		case 0x7e : flag = ZF || (SF != OF); break; 
+		case 0x7f : flag = !ZF && (SF == OF); break; 
+		case 0xe3 : flag = ops_decoded.is_data_size_16 ? !reg_w(R_CX) : !reg_l(R_ECX); break; 
+		default : assert ("missing instr");
+	}
+	if (flag)
 		cpu.eip += (DATA_TYPE_S) op_src->val;
 	/* TODO: Update EFLAGS. */
-//	panic("please implement me");
+	//	panic("please implement me");
 
 	print_asm_template1();
 }
