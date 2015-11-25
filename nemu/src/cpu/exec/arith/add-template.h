5ddd8f142392d0e44c3bf2736a4169a2f2823421
Wu Yuncong <14307130244@fudan.edu.cn>
2015-11-25 20:04:12 +0800

    >  compile NEMU 14307130244 snlia Linux snliaPC 3.13.0-35-generic #62-Ubuntu SMP Fri Aug 15 01:58:42 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  20:04:12 up  8:45,  5 users,  load average: 0.46, 0.55, 0.52 b9092b33921a15083a9d2f7bb2897178a5b8fd4

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -4,7 +4,6 @@
 
 static void do_execute () {
 	DATA_TYPE result = op_dest->val + op_src->val;
-	printf ("%x\n", result);
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -39,7 +39,7 @@ make_group(group1_v,
 
 /* 0x83 */
 make_group(group1_sx_v,
-	sub_si2rm_v, inv, inv, inv, 
+	add_si2rm_v, inv, inv, inv, 
 	inv, sub_si2rm_v, inv, cmp_si2rm_v)
 
 /* 0xc0 */
