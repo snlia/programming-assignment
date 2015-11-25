b12a44b05c6c77159283dcb132c7c63e87c21e1e
Wu Yuncong <14307130244@fudan.edu.cn>
2015-11-25 17:02:07 +0800

    >  compile NEMU 14307130244 snlia Linux snliaPC 3.13.0-35-generic #62-Ubuntu SMP Fri Aug 15 01:58:42 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  17:02:07 up  5:43,  6 users,  load average: 0.39, 0.47, 0.44 e5f9f36ec762ba0235bb31c21eae5c66dac9aae7

---
+++ nemu/src/cpu/exec/data-mov/leave.c
@@ -0,0 +1,39 @@
+#include "cpu/exec/helper.h"
+#include "leave.h"
+
+/* for instruction encoding overloading */
+#define DATA_BYTE 2
+#include "cpu/exec/template-start.h"
+make_helper (leave_w)
+{
+	cpu.esp = cpu.ebp;
+	DATA_TYPE result = swaddr_read(cpu.esp, DATA_BYTE);
+	cpu.esp += DATA_BYTE;
+	MEM_W(R_BP, result);
+
+	print_asm("leave");
+
+	return 1;
+}
+#include "cpu/exec/template-end.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "cpu/exec/template-start.h"
+make_helper (leave_l)
+{
+	cpu.esp = cpu.ebp;
+	DATA_TYPE result = swaddr_read(cpu.esp, DATA_BYTE);
+	cpu.esp += DATA_BYTE;
+	MEM_W(R_EBP, result);
+
+	print_asm("leave");
+
+	return 1;
+}
+#include "cpu/exec/template-end.h"
+#undef DATA_BYTE
+
+make_helper_v (leave);
+
+
---
+++ nemu/src/cpu/exec/data-mov/leave.h
@@ -0,0 +1,9 @@
+#ifndef __LEAVE_H__
+#define __LEAVE_H__
+
+make_helper(leave_w);
+make_helper(leave_l);
+
+make_helper(leave_v);
+
+#endif
