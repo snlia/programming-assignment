d7d4ac4fdf1cc8891dffc6617717840a8a14d8a8
Wu Yuncong <14307130244@fudan.edu.cn>
2015-11-25 16:35:53 +0800

    >  compile NEMU 14307130244 snlia Linux snliaPC 3.13.0-35-generic #62-Ubuntu SMP Fri Aug 15 01:58:42 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:35:53 up  5:17,  5 users,  load average: 0.33, 0.38, 0.40 744b0c1da1f0ae223c4fcf40e8c8f164f526fc

---
+++ nemu/src/cpu/exec/data-mov/leave-template.h
@@ -0,0 +1,16 @@
+#include "cpu/exec/template-start.h"
+
+#define instr leave
+
+static void do_execute () {
+	cpu.esp -= DATA_BYTE;
+	swaddr_write(cpu.esp, DATA_BYTE, (DATA_TYPE) op_src->val);
+	/* TODO: Update EFLAGS. */
+//	panic("please implement me");
+
+	print_asm_template1();
+}
+
+make_instr_helper(r)
+
+#include "cpu/exec/template-end.h"
