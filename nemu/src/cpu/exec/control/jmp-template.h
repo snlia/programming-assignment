94d9bb1f09e074f25352dcbd2c52fd49d302fcab
Wu Yuncong <14307130244@fudan.edu.cn>
2015-11-26 18:40:31 +0800

    >  compile NEMU 14307130244 snlia Linux snliaPC 3.13.0-35-generic #62-Ubuntu SMP Fri Aug 15 01:58:42 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  18:40:31 up 10 min,  4 users,  load average: 0.20, 0.24, 0.23 46eb008293ec13034aef70fb8a326ef6d7685ec2

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -3,10 +3,12 @@
 #define instr jmp
 
 static void do_execute () {
+	printf ("%x\n", cpu.eip);
 	if (op_src->type == OP_TYPE_IMM)
 		cpu.eip += (DATA_TYPE_S) op_src->val;
 	else 
 		cpu.eip = op_src->val;
+	printf ("%x\n", cpu.eip);
 
 	print_asm_template1();
 }
