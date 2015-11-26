389efcfe9d978956bc6890fe7bdfb7c2065cb461
Wu Yuncong <14307130244@fudan.edu.cn>
2015-11-26 19:42:34 +0800

    >  compile NEMU 14307130244 snlia Linux snliaPC 3.13.0-35-generic #62-Ubuntu SMP Fri Aug 15 01:58:42 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  19:42:34 up  1:12,  5 users,  load average: 0.23, 0.14, 0.18 3377ef405c5bde8735be7a3fa2a17d9857c72ec

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -4,7 +4,8 @@
 
 static void do_execute() {
 	DATA_TYPE result = op_dest->val + (DATA_TYPE_S) (-op_src->val);
-	cpu.CF = ((uint64_t) (DATA_TYPE) op_dest->val + (uint64_t) (DATA_TYPE) (-op_src->val))  != result;
+	printf ("%x", (DATA_TYPE) op_src->val);
+	cpu.CF = (!(DATA_TYPE) op_src->val) || (((uint64_t) (DATA_TYPE) op_dest->val + (uint64_t) (DATA_TYPE) (-op_src->val))  != result);
 	cpu.SF = (result >> (DATA_BYTE * 8 - 1)) & 1;
 	cpu.OF = cpu.CF ^ cpu.SF ^ ((op_dest->val >> (DATA_BYTE * 8 - 1)) & 1) ^ (((-op_src->val) >> (DATA_BYTE * 8 - 1)) & 1);
 	cpu.ZF = !result;
