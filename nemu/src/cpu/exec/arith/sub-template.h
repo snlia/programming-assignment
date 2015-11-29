b39486ecbbd18199f4a14e8efbc943e8f0dd9f6c
Wu Yuncong <14307130244@fudan.edu.cn>
2015-11-29 11:14:41 +0800

    >  compile NEMU 14307130244 snlia Linux snliaPC 4.2.0-18-generic #22-Ubuntu SMP Fri Nov 6 18:25:50 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  11:14:41 up 41 min,  5 users,  load average: 0.06, 0.31, 0.45 779b1940eff5bcafaab7f3301f874a3ffd827ffb

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -3,7 +3,7 @@
 #define instr sub
 
 static void do_execute () {
-	DATA_TYPE result = op_dest->val - (DATA_TYPE_S) op_src->val;
+	DATA_TYPE result = op_dest->val + (DATA_TYPE_S) (-op_src->val);
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
