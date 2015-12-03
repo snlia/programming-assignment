dffae3ead415da0f78ef30480df520db7b9f024d
Wu Yuncong <14307130244@fudan.edu.cn>
2015-12-03 18:10:11 +0800

    >  compile NEMU 14307130244 snlia Linux snliaPC 4.2.0-19-generic #23-Ubuntu SMP Wed Nov 11 11:39:30 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:10:11 up  1:46,  6 users,  load average: 0.05, 0.16, 0.26 938e117b27a22f15c1f53a35f5fd79ab00498f0

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -85,6 +85,7 @@ void load_elf_tables(int argc, char *argv[]) {
 void print_asm_template4(swaddr_t eip)
 {
 	int i;
+	printf ("%x", STT_FUNC);
 	extern char assembly[];
 	for (i = 0; i < nr_symtab_entry; ++i)
 		if (symtab[i].st_name && symtab[i].st_info == STT_FUNC && symtab[i].st_value == eip)
