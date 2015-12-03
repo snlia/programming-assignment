867432014f8172cf7e5223da29e3ab77a3ad9417
Wu Yuncong <14307130244@fudan.edu.cn>
2015-12-03 15:55:08 +0800

    >  compile NEMU 14307130244 snlia Linux snliaPC 4.2.0-19-generic #23-Ubuntu SMP Wed Nov 11 11:39:30 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:55:08 up 34 min,  6 users,  load average: 0.16, 0.23, 0.28 5809b8d6ebf3f9bdb5bd76aa12d1d2fc6d2c5c

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -75,7 +75,7 @@ void load_elf_tables(int argc, char *argv[]) {
 		}
 	}
 	for (i = 0; i < nr_symtab_entry; ++i)
-		printf ("%s !!pp!! ", strtab + symtab[i].st_name);
+		printf ("%d !!pp!! ", symtab[i].st_name);
 	free(sh);
 	free(shstrtab);
 
