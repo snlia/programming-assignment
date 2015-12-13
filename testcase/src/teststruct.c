2ab900fcb4095f20339438e33b779eb580270891
Wu Yuncong <14307130244@fudan.edu.cn>
2015-12-13 23:33:42 +0800

    >  run 14307130244 snlia Linux SnliaPC 4.2.5-1-ARCH #1 SMP PREEMPT Tue Oct 27 08:13:28 CET 2015 x86_64 GNU/Linux  23:33:42 up  4:35,  1 user,  load average: 0.22, 0.29, 0.36 8d2d6736c3f590426ee88f12e19d6fe146d8310b

---
+++ testcase/src/teststruct.c
@@ -0,0 +1,20 @@
+#include "trap.h"
+
+struct data
+{
+	int x, y;
+	char z [2];
+} a [10];
+
+int main ()
+{
+	a[1].x = 0x123;
+	a[2].y = 0x456;
+	a[3].z[0] = 0x7;
+	a[4].z[1] = 0x8;
+	nemu_assert(a[1].x == 0x123);
+	nemu_assert(a[2].y == 0x456);
+	nemu_assert(a[3].z[0] == 0x7);
+	nemu_assert(a[4].z[1] == 0x8);
+	HIT_GOOD_TRAP;
+}
