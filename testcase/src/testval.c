511688700c658ca235dbbe106bf62cb39b43fd3c
Wu Yuncong <14307130244@fudan.edu.cn>
2015-12-15 18:34:24 +0800

    >  run 14307130244 snlia Linux SnliaPC 4.2.5-1-ARCH #1 SMP PREEMPT Tue Oct 27 08:13:28 CET 2015 x86_64 GNU/Linux  18:34:24 up 50 min,  1 user,  load average: 0.24, 0.30, 0.27 8d0a240a86ac07c0f012fa7fadf3af58c0a1cb9c

--- testcase/src/testval.c
+++ testcase/src/testval.c
@@ -11,10 +11,5 @@ int main ()
 	a[3] = 4;
 	a[4] = 5;
 	b = 0x01020304;
-	nemu_assert(a[1] == 0x1);
-	nemu_assert(a[2] == 0x2);
-	nemu_assert(a[3] == 0x3);
-	nemu_assert(a[4] == 0x4);
-	HIT_GOOD_TRAP;
 }
 
