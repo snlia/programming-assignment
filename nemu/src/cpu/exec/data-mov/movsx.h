260b3041914750a7f373f643b9d21b5d0973b49d
Wu Yuncong <14307130244@fudan.edu.cn>
2016-01-29 21:08:15 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.3.3-2-ARCH #1 SMP PREEMPT Wed Dec 23 20:09:18 CET 2015 x86_64 GNU/Linux  21:08:15 up  5:44,  1 user,  load average: 1.91, 1.58, 1.53 b767bca62cfa7831e04e2c92db9d03c08cfab909

--- nemu/src/cpu/exec/data-mov/movsx.h
+++ nemu/src/cpu/exec/data-mov/movsx.h
@@ -1,10 +1,10 @@
 #ifndef __MOVSX_H__
 #define __MOVSX_H__
 
-make_helper(rm2rx_w);
-make_helper(rm2rx_l);
-make_helper(rm2rx_v);
-make_helper(rm2rX_l);
+make_helper(movsx_rm2rx_w);
+make_helper(movsx_rm2rx_l);
+make_helper(movsx_rm2rx_v);
+make_helper(movsx_rm2rX_l);
 
 #endif
 
--- nemu/src/cpu/exec/data-mov/movzx.h
+++ nemu/src/cpu/exec/data-mov/movzx.h
@@ -1,10 +1,10 @@
 #ifndef __MOVZX_H__
 #define __MOVZX_H__
 
-make_helper(rm2rx_w);
-make_helper(rm2rx_l);
-make_helper(rm2rx_v);
-make_helper(rm2rX_l);
+make_helper(movzx_rm2rx_w);
+make_helper(movzx_rm2rx_l);
+make_helper(movzx_rm2rx_v);
+make_helper(movzx_rm2rX_l);
 
 #endif
 
