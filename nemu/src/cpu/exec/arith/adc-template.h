094ee6f63d44b2393b3f62ce78d918eb0e67b638
Wu Yuncong <14307130244@fudan.edu.cn>
2016-02-01 12:06:31 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.3.3-2-ARCH #1 SMP PREEMPT Wed Dec 23 20:09:18 CET 2015 x86_64 GNU/Linux  12:06:31 up  1:35,  1 user,  load average: 0.37, 0.29, 0.24 9853075691bb9c06cc2362b0340af9e7d52979c1

---
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -0,0 +1,33 @@
+#include "cpu/exec/template-start.h"
+
+#define instr adc
+
+static void do_execute () {
+	DATA_TYPE result = op_dest->val + op_src->val + (DATA_TYPE) cpu.CF;
+	OPERAND_W(op_dest, result);
+
+	/* TODO: Update EFLAGS. */
+//	panic("please implement me");
+	cpu.CF = ((uint64_t) (DATA_TYPE) op_src->val + (uint64_t) (DATA_TYPE) op_dest->val + (uint64_t) (DATA_TYPE) cpu.CF) != result;
+	uint32_t off = (DATA_BYTE * 8 - 1);
+	cpu.SF = (result >> off) & 1;
+	cpu.OF = (((op_src->val >> off) & 1) ^ ((op_dest->val >> off) & 1) ^ 1) && (cpu.SF ^ ((op_dest->val >> off) & 1));
+	cpu.ZF = !result;
+	cpu.AF = ((op_src->val & 0x7) + (op_dest->val & 0x7)) < 0x8;
+	result = result & 0xff;
+	result = (result ^ result) & 0xf;
+	result = (result ^ result) & 0x3;
+	cpu.PF = (1 ^ result ^ result) & 1;
+
+	print_asm_template2();
+}
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
