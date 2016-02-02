da894128c397c23930fc389153b410c84c5b6b49
Wu Yuncong <14307130244@fudan.edu.cn>
2016-02-02 13:54:57 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.3.3-2-ARCH #1 SMP PREEMPT Wed Dec 23 20:09:18 CET 2015 x86_64 GNU/Linux  13:54:57 up 42 min,  1 user,  load average: 0.06, 0.03, 0.06 620358861d9f45d0ff3eaa3cba0999925d400a6

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -90,7 +90,7 @@ void print_asm_template4(swaddr_t eip, char * S)
     printf ("%x\n", eip);
 	extern char assembly[];
 	for (i = 0; i < nr_symtab_entry; ++i)
-		if (symtab[i].st_name && symtab[i].st_info == FUNC && symtab[i].st_value == eip)
+		if (symtab[i].st_name /*&& symtab[i].st_info == FUNC*/ && symtab[i].st_value == eip)
         {
 			Assert(snprintf(assembly, 80, "%s %x <%s>", S, eip, strtab + symtab[i].st_name) < 80, "buffer overflow!");
         }
