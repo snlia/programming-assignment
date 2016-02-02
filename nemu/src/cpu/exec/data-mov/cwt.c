cfed48e81e9c5d561185b0b1cff9f4e09ad59faa
Wu Yuncong <14307130244@fudan.edu.cn>
2016-02-02 14:48:12 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.3.3-2-ARCH #1 SMP PREEMPT Wed Dec 23 20:09:18 CET 2015 x86_64 GNU/Linux  14:48:12 up 38 min,  1 user,  load average: 0.75, 0.87, 0.82 3b69c60487db90e02bcabd8b4515bcd9e033e4e

---
+++ nemu/src/cpu/exec/data-mov/cwt.c
@@ -0,0 +1,23 @@
+#include "cpu/exec/helper.h"
+#include "leave.h"
+
+make_helper (cwt_w)
+{
+    reg_w (R_AX) = (int8_t) reg_b (R_AL);
+
+	print_asm("cwt");
+	return 1;
+}
+
+make_helper (cwt_l)
+{
+    reg_l (R_EAX) = (int16_t) reg_w (R_AX);
+
+	print_asm("cwt");
+	return 1;
+}
+
+make_helper_v (cwt);
+
+
+
