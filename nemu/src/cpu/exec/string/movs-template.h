656c9d024731e1c3a993130eb392d182dacd42ed
Wu Yuncong <14307130244@fudan.edu.cn>
2016-02-02 11:24:05 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.3.3-2-ARCH #1 SMP PREEMPT Wed Dec 23 20:09:18 CET 2015 x86_64 GNU/Linux  11:24:05 up  1:17,  1 user,  load average: 1.26, 0.97, 0.84 6fc44037a89f4a67d98ef5aaafdda075e0bb46a5

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -1,18 +1,26 @@
 make_helper (concat (movs_, SUFFIX))
 {
-	DATA_TYPE result = swaddr_read(REG (R_ESI), DATA_BYTE);
-    swaddr_write (REG (R_EDI), DATA_BYTE, result);
+#if DATA_TYPE == 2
+#define DESTindex (reg_w(R_DI))
+#define SRCindex (reg_w(R_SI))
+#else
+#define DESTindex (reg_l(R_EDI))
+#define SRCindex (reg_l(R_ESI))
+#endif
+	DATA_TYPE result = swaddr_read(SRCindex, DATA_BYTE);
+    swaddr_write (DESTindex, DATA_BYTE, result);
     if (cpu.DF) 
     {
-        REG (R_ESI) -= DATA_BYTE;
-        REG (R_EDI) -= DATA_BYTE;
+        SRCindex -= DATA_BYTE;
+        DESTindex -= DATA_BYTE;
     }
     else
     {
-        REG (R_ESI) += DATA_BYTE;
-        REG (R_EDI) += DATA_BYTE;
+        SRCindex += DATA_BYTE;
+        DESTindex += DATA_BYTE;
     }
 	print_asm("movs");
-
+#undef DESTindex
+#undef SRCindex
 	return 1;
 }
--- nemu/src/cpu/exec/string/movs.c
+++ nemu/src/cpu/exec/string/movs.c
@@ -3,6 +3,12 @@
 
 /* for instruction encoding overloading */
 
+#define DATA_BYTE 1
+#include "cpu/exec/template-start.h"
+#include "movs-template.h"
+#include "cpu/exec/template-end.h"
+#undef DATA_BYTE
+
 #define DATA_BYTE 2
 #include "cpu/exec/template-start.h"
 #include "movs-template.h"
@@ -11,24 +17,6 @@
 
 #define DATA_BYTE 4
 #include "cpu/exec/template-start.h"
-make_helper (movs_b)
-{
-	DATA_TYPE result = swaddr_read(REG (R_ESI), 1);
-    swaddr_write (REG (R_EDI), 1, result);
-    if (cpu.DF) 
-    {
-        REG (R_ESI) -= 1;
-        REG (R_EDI) -= 1;
-    }
-    else
-    {
-        REG (R_ESI) += 1;
-        REG (R_EDI) += 1;
-    }
-	print_asm("movs");
-
-	return 1;
-}
 #include "movs-template.h"
 #include "cpu/exec/template-end.h"
 #undef DATA_BYTE
