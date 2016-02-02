1edb7ab8c9d792bdb95923dc51aa537fe5a109b5
Wu Yuncong <14307130244@fudan.edu.cn>
2016-02-02 14:28:41 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.3.3-2-ARCH #1 SMP PREEMPT Wed Dec 23 20:09:18 CET 2015 x86_64 GNU/Linux  14:28:41 up 19 min,  1 user,  load average: 0.46, 0.67, 0.56 aa75f30f5d1284036b6cbd90d715e7122dd1287c

--- nemu/src/cpu/exec/prefix/prefix.c
+++ nemu/src/cpu/exec/prefix/prefix.c
@@ -6,5 +6,6 @@ make_helper(data_size) {
 	ops_decoded.is_data_size_16 = true;
 	int instr_len = exec(eip + 1);
 	ops_decoded.is_data_size_16 = false;
+    printf ("%d\n", instr_len + 1);
 	return instr_len + 1;
 }
