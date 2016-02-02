c8106682716db49b46f653e05781cdcc0e5e8370
Wu Yuncong <14307130244@fudan.edu.cn>
2016-02-02 11:24:31 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.3.3-2-ARCH #1 SMP PREEMPT Wed Dec 23 20:09:18 CET 2015 x86_64 GNU/Linux  11:24:31 up  1:17,  1 user,  load average: 1.24, 0.99, 0.85 bc0e395cac8bb70794c42f9486d4109a8487d4f

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -1,5 +1,3 @@
-make_helper (concat (movs_, SUFFIX))
-{
 #if DATA_TYPE == 2
 #define DESTindex (reg_w(R_DI))
 #define SRCindex (reg_w(R_SI))
@@ -7,6 +5,9 @@ make_helper (concat (movs_, SUFFIX))
 #define DESTindex (reg_l(R_EDI))
 #define SRCindex (reg_l(R_ESI))
 #endif
+
+make_helper (concat (movs_, SUFFIX))
+{
 	DATA_TYPE result = swaddr_read(SRCindex, DATA_BYTE);
     swaddr_write (DESTindex, DATA_BYTE, result);
     if (cpu.DF) 
@@ -20,7 +21,8 @@ make_helper (concat (movs_, SUFFIX))
         DESTindex += DATA_BYTE;
     }
 	print_asm("movs");
-#undef DESTindex
-#undef SRCindex
 	return 1;
 }
+
+#undef DESTindex
+#undef SRCindex
