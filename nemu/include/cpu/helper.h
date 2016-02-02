3d61edf27356cb30f6c0e2e0def6816e4b676878
Wu Yuncong <14307130244@fudan.edu.cn>
2016-02-02 14:32:22 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.3.3-2-ARCH #1 SMP PREEMPT Wed Dec 23 20:09:18 CET 2015 x86_64 GNU/Linux  14:32:22 up 22 min,  1 user,  load average: 0.79, 0.84, 0.66 4919cb1fc7fd91042c598a54f88f9c7010d855d4

--- nemu/include/cpu/helper.h
+++ nemu/include/cpu/helper.h
@@ -15,8 +15,8 @@ static inline uint32_t instr_fetch(swaddr_t addr, size_t len) {
 static inline int idex(swaddr_t eip, int (*decode)(swaddr_t), void (*execute) (void)) {
 	/* eip is pointing to the opcode */
 	int len = decode(eip + 1);
-    printf ("%d\n", len + 1);
 	cpu.eip += len + 1;
+    printf ("%x\n", cpu.eip);
 	execute();
 	return 0;	// "1" for opcode
 }
