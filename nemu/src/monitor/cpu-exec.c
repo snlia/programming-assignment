6607b9b006ae55910a0b488a19eb9c7e334314ec
Wu Yuncong <14307130244@fudan.edu.cn>
2016-02-02 14:34:48 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.3.3-2-ARCH #1 SMP PREEMPT Wed Dec 23 20:09:18 CET 2015 x86_64 GNU/Linux  14:34:48 up 25 min,  1 user,  load average: 0.51, 0.72, 0.64 656b309a797190735d5e286fbcd8d144d15487

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -61,7 +61,7 @@ void cpu_exec(volatile uint32_t n) {
 		/* Execute one instruction, including instruction fetch,
 		 * instruction decode, and the actual execution. */
 		int instr_len = exec(cpu.eip);
-        printf ("!!!%x\n", instr_len);
+        printf ("!!!%x %x\n", instr_len, cpu.eip);
 
 		cpu.eip += instr_len;
 
