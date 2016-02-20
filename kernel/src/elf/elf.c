34e7f2d2921b0e44dc51e8ad10d05b6a3bd0c5a0
Wu Yuncong <14307130244@fudan.edu.cn>
2016-02-20 22:56:25 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.4.1-2-ARCH #1 SMP PREEMPT Wed Feb 3 13:12:33 UTC 2016 x86_64 GNU/Linux  22:56:25 up  6:24,  1 user,  load average: 0.03, 0.14, 0.20 ec67dc1d4bcbf902f497ce1443ed5dd5478fcc5e

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -4,6 +4,7 @@
 #include <elf.h>
 
 #define ELF_OFFSET_IN_DISK 0
+#define PHSIZE 56
 
 #ifdef HAS_DEVICE
 void ide_read(uint8_t *, uint32_t, uint32_t);
@@ -21,6 +22,7 @@ uint32_t loader() {
 	Elf32_Phdr *ph = NULL;
 
 	uint8_t buf[4096];
+    int i;
 
 #ifdef HAS_DEVICE
 	ide_read(buf, ELF_OFFSET_IN_DISK, 4096);
@@ -34,43 +36,47 @@ uint32_t loader() {
 	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
-
-	/* Load each program segment */
-	panic("please implement me");
-	for(; true; ) {
-		/* Scan the program header table, load each segment into memory */
-		if(ph->p_type == PT_LOAD) {
-
-			/* TODO: read the content of the segment from the ELF file 
-			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
-			 */
-			 
-			 
-			/* TODO: zero the memory region 
-			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
-			 */
+    //e_phoff e_phnum
+    Elf32_Off Off = elf->phoff;
+    uint16_t phnum = elf->phnum;
+    /* Load each program segment */
+    while (phnum--){
+        /* Scan the program header table, load each segment into memory */
+        ph = (void *) (buf + Off);
+        Off += sizeof (ph);
+        if(ph->p_type == PT_LOAD) {
+            /* TODO: read the content of the segment from the ELF file 
+             * to the memory region [VirtAddr, VirtAddr + FileSiz)
+             */
+            for (i = 0; i < ph->p_filesz; ++i)
+                swaddr_write (ph->p_vaddr + i, 1, buf[ph->p_offset + i]);
+            /* TODO: zero the memory region 
+             * [VirtAddr + FileSiz, VirtAddr + MemSiz)
+             */
+            for (i = ph->p_filesz; i < ph->p_memsz; ++i)
+                swaddr_write (ph->p_vaddr + i, 1, 0);
 
 
 #ifdef IA32_PAGE
-			/* Record the program break for future use. */
-			extern uint32_t brk;
-			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
-			if(brk < new_brk) { brk = new_brk; }
+            /* Record the program break for future use. */
+            extern uint32_t brk;
+            uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
+            if(brk < new_brk) { brk = new_brk; }
 #endif
-		}
-	}
+        }
+    }
 
-	volatile uint32_t entry = elf->e_entry;
+    volatile uint32_t entry = elf->e_entry;
 
 #ifdef IA32_PAGE
-	mm_malloc(KOFFSET - STACK_SIZE, STACK_SIZE);
+    mm_malloc(KOFFSET - STACK_SIZE, STACK_SIZE);
 
 #ifdef HAS_DEVICE
-	create_video_mapping();
+    create_video_mapping();
 #endif
 
-	write_cr3(get_ucr3());
+    write_cr3(get_ucr3());
 #endif
 
-	return entry;
+    return entry;
 }
