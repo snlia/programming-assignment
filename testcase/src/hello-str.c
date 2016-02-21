aedcc5b3e194d67fcd52db430bacbf2683ee899f
Wu Yuncong <14307130244@fudan.edu.cn>
2016-02-21 16:42:41 +0800

    >  run 14307130244 snlia Linux SnliaPC 4.4.1-2-ARCH #1 SMP PREEMPT Wed Feb 3 13:12:33 UTC 2016 x86_64 GNU/Linux  16:42:41 up  1:36,  1 user,  load average: 0.02, 0.09, 0.12 aa23819729b98aefc5883b4a54a5f942ba34966

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -15,6 +15,7 @@ void *sbrk(int incr) {
 
 int main() {
 	sprintf(buf, "%s", "Hello world!\n");
+	HIT_GOOD_TRAP;
 	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
 	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
