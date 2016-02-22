6b423054a02835e05805a1bbd1dd9a6b1291e4d9
Wu Yuncong <14307130244@fudan.edu.cn>
2016-02-22 18:35:41 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.4.1-2-ARCH #1 SMP PREEMPT Wed Feb 3 13:12:33 UTC 2016 x86_64 GNU/Linux  18:35:41 up  2:00,  1 user,  load average: 0.89, 0.58, 0.46 859ca978fcefad85975e110176366baa9796eeb

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -141,7 +141,7 @@ void init_regex() {
 		ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED | REG_ICASE); // change here : use REG_ICASE option to be case insensitive
 		if(ret != 0) {
 			regerror(ret, &re[i], error_msg, 128);
-			Assert(ret != 0, "regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+			Assert(ret == 0, "regex compilation failed: %s\n%s", error_msg, rules[i].regex);
 		}
 	}
 }
