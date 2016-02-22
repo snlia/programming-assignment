88d6cc361a60f49fb68ce7481b561486bbf762b0
Wu Yuncong <14307130244@fudan.edu.cn>
2016-02-22 18:49:48 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.4.1-2-ARCH #1 SMP PREEMPT Wed Feb 3 13:12:33 UTC 2016 x86_64 GNU/Linux  18:49:48 up  2:15,  1 user,  load average: 0.12, 0.52, 0.56 63b4e33be4720791263d6cc8628d7b3be86514

--- nemu/src/cpu/exec/arith/adc.h
+++ nemu/src/cpu/exec/arith/adc.h
@@ -2,20 +2,12 @@
 #define __ADC_H__
 
 make_helper(adc_i2a_b);
-make_helper(adc_i2a_w);
-make_helper(adc_i2a_l);
 
 make_helper(adc_i2rm_b);
-make_helper(adc_i2rm_w);
-make_helper(adc_i2rm_l);
 
 make_helper(adc_r2rm_b);
-make_helper(adc_r2rm_w);
-make_helper(adc_r2rm_l);
 
 make_helper(adc_rm2r_b);
-make_helper(adc_rm2r_w);
-make_helper(adc_rm2r_l);
 
 make_helper(adc_si2rm_v);
 make_helper(adc_i2a_v);
