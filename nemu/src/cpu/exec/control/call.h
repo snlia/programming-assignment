b12998f8a8195c748b6e62998e1185923133bc7d
Wu Yuncong <14307130244@fudan.edu.cn>
2016-02-22 18:51:27 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.4.1-2-ARCH #1 SMP PREEMPT Wed Feb 3 13:12:33 UTC 2016 x86_64 GNU/Linux  18:51:27 up  2:16,  1 user,  load average: 0.28, 0.45, 0.53 3ff0bf9ba7d83675db801bb745a291a1eadf4b07

--- nemu/src/cpu/exec/control/call.h
+++ nemu/src/cpu/exec/control/call.h
@@ -1,12 +1,6 @@
 #ifndef __CALL_H__
 #define __CALL_H__
 
-make_helper(call_i_w);
-make_helper(call_i_l);
-
-make_helper(call_rm_w);
-make_helper(call_rm_l);
-
 make_helper(call_i_v);
 make_helper(call_rm_v);
 
