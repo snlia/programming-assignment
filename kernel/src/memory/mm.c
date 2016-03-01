d12ee98015139c57fd681a96058acd45b7ab152e
Wu Yuncong <14307130244@fudan.edu.cn>
2016-03-01 20:33:51 +0800

    >  compile kernel 14307130244 snlia Linux SnliaPC 4.4.1-2-ARCH #1 SMP PREEMPT Wed Feb 3 13:12:33 UTC 2016 x86_64 GNU/Linux  20:33:51 up  2:27,  1 user,  load average: 0.13, 0.25, 0.31 fc00d6524f7f7a35eb6bfa3512f55c74e117441a

--- kernel/src/memory/mm.c
+++ kernel/src/memory/mm.c
@@ -5,10 +5,10 @@
 static PDE updir[NR_PDE] align_to_page;
 static CR3 ucr3;
 
-inline PDE* get_updir() { return updir; }
-inline uint32_t get_ucr3() { return ucr3.val; }
+extern inline PDE* get_updir() { return updir; }
+extern inline uint32_t get_ucr3() { return ucr3.val; }
 
-inline PDE* get_kpdir();
+PDE* get_kpdir();
 
 uint32_t brk = 0;
 
