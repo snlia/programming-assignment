37080e015f8f4beaa2d06bdb012afee984fed5ea
Wu Yuncong <14307130244@fudan.edu.cn>
2016-03-31 21:15:24 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.4.5-1-ARCH #1 SMP PREEMPT Thu Mar 10 07:38:19 CET 2016 x86_64 GNU/Linux  21:15:24 up 26 min,  1 user,  load average: 0.15, 0.13, 0.14 5d9ffec6b395568f5b228889f30872a838abb8e5

--- nemu/src/memory/dram.c
+++ nemu/src/memory/dram.c
@@ -53,7 +53,7 @@ void init_ddr3() {
 
 static void ddr3_read(hwaddr_t addr, void *data) {
 	Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!", addr);
-#ifdef O1
+#ifdef O2
     memcpy (data, hw_mem + (addr & 0x7fffff0), BURST_LEN);
     printf ("%x %x\n", addr, *((uint32_t *)data));
 #else
