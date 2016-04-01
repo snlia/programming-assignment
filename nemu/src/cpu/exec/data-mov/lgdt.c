05d3b12bb7629298a3843bf12c518ba1d5d0a534
Wu Yuncong <14307130244@fudan.edu.cn>
2016-04-01 11:29:45 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.4.5-1-ARCH #1 SMP PREEMPT Thu Mar 10 07:38:19 CET 2016 x86_64 GNU/Linux  11:29:45 up  2:29,  1 user,  load average: 1.10, 1.10, 1.04 5673d04d61cf825d84edee1556a1f649d1aa406

--- nemu/src/cpu/exec/data-mov/lgdt.c
+++ nemu/src/cpu/exec/data-mov/lgdt.c
@@ -1,21 +1,14 @@
 #include "cpu/exec/helper.h"
 
-#define DATA_BYTE 1
-#include "lgdt-template.h"
-#undef DATA_BYTE
-
-#define DATA_BYTE 2
-#include "lgdt-template.h"
-#undef DATA_BYTE
-
 #define DATA_BYTE 4
-#include "lgdt-template.h"
+#include "cpu/exec/template-start.h"
+#define instr lgdt
+
+static void do_execute () {
+    print_asm_template1 ();
+}
+
+make_instr_helper (i);
+#include "cpu/exec/template-end.h"
 #undef DATA_BYTE
-
-/* for instruction encoding overloading */
-
-make_helper_v (lgdt_r)
-make_helper_v (lgdt_rm)
-make_helper_v (lgdt_i)
-
 
--- nemu/src/cpu/exec/data-mov/lgdt.h
+++ nemu/src/cpu/exec/data-mov/lgdt.h
@@ -1,11 +1,6 @@
 #ifndef __LGDT_H__
 #define __LGDT_H__
 
-make_helper(lgdt_i_b);
 make_helper(lgdt_i_l);
-
-make_helper(lgdt_r_v);
-make_helper(lgdt_rm_v);
-make_helper(lgdt_i_v);
 
 #endif
