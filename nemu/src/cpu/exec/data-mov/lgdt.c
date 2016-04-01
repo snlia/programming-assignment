106e66ce1e6db895aa0763354e5feccf60b5ac7d
Wu Yuncong <14307130244@fudan.edu.cn>
2016-04-01 10:01:37 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.4.5-1-ARCH #1 SMP PREEMPT Thu Mar 10 07:38:19 CET 2016 x86_64 GNU/Linux  10:01:37 up  1:01,  1 user,  load average: 0.21, 0.43, 0.43 b3fe8e2a6cf1982c3ec7f61a72680a3795215

--- nemu/src/cpu/exec/data-mov/lgdt.c
+++ nemu/src/cpu/exec/data-mov/lgdt.c
@@ -1,2 +1,13 @@
 #include "cpu/exec/helper.h"
 #include "lgdt.h"
+
+#define DATA_BYTE 4
+#include "cpu/exec/template-start.h"
+#define instr lgdt
+void static do_execute () {
+    print_asm ("lgdt");
+}
+make_instr_helper (i);
+
+#include "cpu/exec/template-end.h"
+#undef DATA_BYTE
