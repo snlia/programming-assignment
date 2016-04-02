170bf7d6aa4c6247fd6ae101ba5aa3e4f87478e3
Wu Yuncong <14307130244@fudan.edu.cn>
2016-04-02 13:06:23 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.4.5-1-ARCH #1 SMP PREEMPT Thu Mar 10 07:38:19 CET 2016 x86_64 GNU/Linux  13:06:23 up 7 min,  1 user,  load average: 0.12, 0.62, 0.40 88e82ceae28a0029aa6689d9759eaa57c9182bad

---
+++ nemu/include/cpu/seg.h
@@ -0,0 +1,29 @@
+#ifndef __SEG_H__
+#define __SEG_H__
+
+#include "common.h"
+
+typedef union {
+    struct {
+        uint16_t limit0;
+        uint16_t base0;
+        uint8_t base1;
+        uint8_t A : 1;
+        uint8_t TYPE : 3;
+        uint8_t ONE : 1;
+        uint8_t DPL : 2;
+        uint8_t P : 1;
+        uint8_t limit1 : 4;
+        uint8_t AVL : 1;
+        uint8_t O : 1;
+        uint8_t X : 1;
+        uint8_t G : 1;
+        uint8_t base2;
+    };
+    uint64_t val;
+} SEG_des;
+
+#define seg_base(x) ((x).base0 | ((x).base1 << 16) | ((x).base2 << 24))
+#define seg_limit(x) ((x).limit0 | ((x).limit1 << 16))
+
+#endif
--- nemu/src/cpu/exec/control/ljmp.c
+++ nemu/src/cpu/exec/control/ljmp.c
@@ -1,7 +1,14 @@
 #include "cpu/exec/helper.h"
 #include "ljmp.h"
+#include "cpu/seg.h"
+
+extern uint32_t lnaddr_read(lnaddr_t , size_t );
 
 make_helper (ljmp) {
-    printf ("%x %x\n", instr_fetch (eip + 1, 4), instr_fetch (eip + 5, 2));
+//    swaddr_t addr = instr_fetch (eip + 1, 4);
+    uint16_t off = instr_fetch (eip + 5, 2);
+    SEG_des seg_d;
+    seg_d.val = lnaddr_read (off + cpu.GDTR_B, 4) | ((uint64_t) lnaddr_read (off + cpu.GDTR_B + 4, 4) << 32);
+    printf ("%lx",seg_d.val);
     return 7;
 }
