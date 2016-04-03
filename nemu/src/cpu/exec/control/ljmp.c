819358795eef1fc10bbdde70eb28ae85424862f0
Wu Yuncong <14307130244@fudan.edu.cn>
2016-04-03 17:43:38 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.4.5-1-ARCH #1 SMP PREEMPT Thu Mar 10 07:38:19 CET 2016 x86_64 GNU/Linux  17:43:38 up  4:00,  1 user,  load average: 0.44, 0.46, 0.42 6d352d0a992ef4a39896f2d9fb88df8eb392d233

--- nemu/src/cpu/exec/control/ljmp.c
+++ nemu/src/cpu/exec/control/ljmp.c
@@ -1,20 +1,19 @@
 #include "cpu/exec/helper.h"
 #include "ljmp.h"
 #include "cpu/seg.h"
+#include "cpu/reg.h"
 
 extern uint32_t lnaddr_read(lnaddr_t , size_t );
 
 make_helper (ljmp) {
-//    swaddr_t addr = instr_fetch (eip + 1, 4);
+    swaddr_t addr = instr_fetch (eip + 1, 4);
     uint16_t off = instr_fetch (eip + 5, 2);
     SEG_des seg_d;
     seg_d.val = lnaddr_read (off + cpu.GDTR_B, 4) | ((uint64_t) lnaddr_read (off + cpu.GDTR_B + 4, 4) << 32);
-    switch (seg_d.TYPE) {
-        case 4:
-            break;
-        case 5:
-            break;
-        default : assert (0);
+    if (seg_d.TYPE & 4) {
+        cpu.eip = addr;
+        cpu.CS = off;
     }
+    else assert (0);
     return 7;
 }
