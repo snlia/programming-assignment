aed83ffe44ed4efbc4777e919acf4a086fd1c52b
Wu Yuncong <14307130244@fudan.edu.cn>
2016-04-05 11:11:18 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.4.5-1-ARCH #1 SMP PREEMPT Thu Mar 10 07:38:19 CET 2016 x86_64 GNU/Linux  11:11:18 up  1:24,  1 user,  load average: 0.13, 0.16, 0.14 617852f2b45964cba697aa31905724dd8a8fe73c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -36,7 +36,8 @@ lnaddr_t seg_translate (swaddr_t swaddr) {
 #ifdef DEBUG
 	assert (!(cpu.CR[0] & 1) || (cpu.spr[current_sreg].index << 3) < cpu.GDTR_L);
 #endif
-    printf ("%x %x\n", (seg_limit (cache_SEG[current_sreg]) << 12), swaddr);
+    printf ("%x\n", swaddr);
+//    printf ("%x %x\n", (seg_limit (cache_SEG[current_sreg]) << 12), swaddr);
     assert ((seg_limit (cache_SEG[current_sreg]) << 12) > swaddr);
     return swaddr + seg_base (cache_SEG[current_sreg]);
 }
