d5d14ff8ab97172bc7a5e987e34a47b689ac8d84
Wu Yuncong <14307130244@fudan.edu.cn>
2016-04-05 11:11:47 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.4.5-1-ARCH #1 SMP PREEMPT Thu Mar 10 07:38:19 CET 2016 x86_64 GNU/Linux  11:11:47 up  1:24,  1 user,  load average: 0.13, 0.16, 0.14 dc4adce5826bd08ed4f8d8c98f12b4a09749ae01

--- nemu/src/cpu/exec/control/ljmp.c
+++ nemu/src/cpu/exec/control/ljmp.c
@@ -7,6 +7,7 @@ extern uint32_t lnaddr_read(lnaddr_t , size_t );
 
 make_helper (ljmp) {
     swaddr_t addr = instr_fetch (eip + 1, 4);
+    puts ("!!!");
     uint16_t off = instr_fetch (eip + 5, 2);
     cache_SEG[1].val = lnaddr_read (off + cpu.GDTR_B, 4) | ((uint64_t) lnaddr_read (off + cpu.GDTR_B + 4, 4) << 32);
     if (cache_SEG[1].TYPE & 4) {
