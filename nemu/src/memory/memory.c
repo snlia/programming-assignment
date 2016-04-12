7bf63f977694749c8e913466010fa5099f64de40
Wu Yuncong <14307130244@fudan.edu.cn>
2016-04-12 12:19:09 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.4.5-1-ARCH #1 SMP PREEMPT Thu Mar 10 07:38:19 CET 2016 x86_64 GNU/Linux  12:19:09 up  2:05,  1 user,  load average: 0.19, 0.16, 0.15 5675fd201cbcd0ae35e2b40554f5cea4ec66d62b

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,7 +12,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 #ifdef CACHE 
     return L1_read (addr, len);
 #else
-	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+    return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 #endif
 }
 
@@ -20,28 +20,50 @@ void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 #ifdef CACHE 
     L1_write (addr, len, data);
 #else
-	dram_write(addr, len, data);
+    dram_write(addr, len, data);
 #endif
 }
 
+hwaddr_t page_translate (lnaddr_t addr) {
+    if (!cpu.CR0.protect_enable || !cpu.CR0.paging)
+        return addr;
+    return addr;
+}
+
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
-	return hwaddr_read(addr, len);
+    assert(len == 1 || len == 2 || len == 4);
+    if (0) {
+        /* this is a special case, you can handle it later. */
+        assert(0);
+    }
+    else {
+        hwaddr_t hwaddr = page_translate(addr);
+        return hwaddr_read(hwaddr, len);
+    }
 }
 
 void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
-	hwaddr_write(addr, len, data);
+    assert(len == 1 || len == 2 || len == 4);
+    if (0) {
+        /* this is a special case, you can handle it later. */
+        assert(0);
+    }
+    else {
+        hwaddr_t hwaddr = page_translate(addr);
+        return hwaddr_write(hwaddr, len, data);
+    }
 }
 
 lnaddr_t seg_translate (swaddr_t swaddr, uint8_t sreg) {
     if (!cpu.CR0.protect_enable) return swaddr;
-	assert (!(cpu.CR0.protect_enable) || (cpu.spr[sreg].index << 3) < cpu.GDTR_L);
+    assert (!(cpu.CR0.protect_enable) || (cpu.spr[sreg].index << 3) < cpu.GDTR_L);
     assert ((seg_limit (cache_SEG[sreg]) << 12) > swaddr);
     return swaddr + seg_base (cache_SEG[sreg]);
 }
 
 uint32_t swaddr_read(swaddr_t addr, size_t len, uint8_t sreg) {
 #ifdef DEBUG
-	assert(len == 1 || len == 2 || len == 4);
+    assert(len == 1 || len == 2 || len == 4);
 #endif
 #ifndef O1 
     addr = seg_translate(addr, sreg);
