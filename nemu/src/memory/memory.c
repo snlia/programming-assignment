78e8bc4629758cc5fddacd97365a1d8f74c88dac
Wu Yuncong <14307130244@fudan.edu.cn>
2016-04-15 14:18:16 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.5.0-1-ARCH #1 SMP PREEMPT Tue Mar 15 09:41:03 CET 2016 x86_64 GNU/Linux  14:18:16 up  1:47,  1 user,  load average: 0.20, 0.15, 0.22 57da967c3a4082b9e5f79fc817a53335e0ab2f59

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -59,7 +59,6 @@ hwaddr_t page_translate (lnaddr_t addr) {
     if (!cpu.CR0.protect_enable || !cpu.CR0.paging) return addr;
     PDE pde;
     pde.val = hwaddr_read (((addr >> 20) & 0xffc) | (cpu.CR3.page_directory_base) << 12, 4);
-    printf ("%x!!!\n", ((addr >> 20) & 0xffc) + (cpu.CR3.page_directory_base << 12));
     if (!pde.present) assert (0);
     PTE pte;
     pte.val = hwaddr_read (((addr >> 10) & 0xffc) | (pde.page_frame << 12), 4);
