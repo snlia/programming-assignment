4067d33120066ab5c50f0d84593d7e6ce233ef01
Wu Yuncong <14307130244@fudan.edu.cn>
2016-04-25 14:43:07 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  14:43:07 up  2:10,  1 user,  load average: 0.09, 0.10, 0.13 4cf92ff7bc56ac35e283f9779d1744a31d3454ad

--- nemu/src/memory/tlb.c
+++ nemu/src/memory/tlb.c
@@ -61,10 +61,10 @@ PTE TLB_read (lnaddr_t addr) {
     for (int i = 0; i < SET_SIZE; ++i) if (TLBset[i].valid && TLBset[i].tag == tmp.tag)
         return TLBset[i].val;
     PDE pde;
-    pde.val = hwaddr_read (((addr >> 20) & 0xffc) | (cpu.CR3.page_directory_base) << 12, 4);
+    pde.val = hwaddr_read (((addr >> 20) & 0xffc) + ((cpu.CR3.page_directory_base) << 12), 4);
     if (!pde.present) assert (0);
     PTE pte;
-    pte.val = hwaddr_read (((addr >> 10) & 0xffc) | (pde.page_frame << 12), 4);
+    pte.val = hwaddr_read (((addr >> 10) & 0xffc) + (pde.page_frame << 12), 4);
     if (!pte.present) assert (0);
     for (int i = 0; i < SET_SIZE; ++i) if (!TLBset[i].valid) {
         TLBset[i].val = pte;
