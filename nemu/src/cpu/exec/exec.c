25625094da65a15f7ea7e26886127bc916e840e1
Wu Yuncong <14307130244@fudan.edu.cn>
2016-04-26 18:45:50 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  18:45:49 up 49 min,  1 user,  load average: 1.95, 1.61, 1.25 7da89cab8dcd63986f0b0ddc905b16cbdf2af364

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -239,6 +239,7 @@ helper_fun _2byte_opcode_table [256] = {
 
 make_helper(exec) {
 	ops_decoded.opcode = instr_fetch(eip, 1);
+    if (ops_decoded.opcode > 0) printf ("= =? %x\n", instr_fetch (eip, 1));
 	return opcode_table[ ops_decoded.opcode ](eip);
 }
 
