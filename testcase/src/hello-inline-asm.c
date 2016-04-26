002a2b8392dd8d2f6121e9c8460b31da38081d5d
Wu Yuncong <14307130244@fudan.edu.cn>
2016-04-26 08:30:13 +0800

    >  run 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  08:30:13 up 52 min,  1 user,  load average: 0.05, 0.13, 0.13 e4f4410942b4d3f28972ff716d449c30ce5adf67

--- testcase/src/hello-inline-asm.c
+++ testcase/src/hello-inline-asm.c
@@ -6,8 +6,8 @@ int main() {
 	asm volatile (  "movl $4, %eax;"	// system call ID, 4 = SYS_write
 					"movl $1, %ebx;"	// file descriptor, 1 = stdout
 					"movl $str, %ecx;"	// buffer address
-					"movl $14, %edx;"	// length
                     "int3;"
+					"movl $14, %edx;"	// length
 					"int $0x80");
 
 	HIT_GOOD_TRAP;
