09e62261eca195742564c235858ddbeebd9bd3f3
Wu Yuncong <14307130244@fudan.edu.cn>
2016-04-26 14:39:43 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  14:39:43 up  7:01,  1 user,  load average: 0.00, 0.06, 0.08 344481a81bbdd2f9dc607cb2c2c32bd431b6363a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -298,12 +298,12 @@ void ui_mainloop() {
         }
         else strcpy (preargs, str);
 
+        printf ("%s\n", str);
         char *cmd = strtok(str, " ");
 
 		/* treat the remaining string as the arguments,
 		 * which may need further parsing
 		 */
-        puts ("ppp");
 		char *args = cmd + strlen(cmd) + 1;
 		if(args >= str_end) {
 			args = NULL;
