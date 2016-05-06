3b3f448c970446dd8fb85edbc06fb6d7f5666af2
Wu Yuncong <14307130244@fudan.edu.cn>
2016-05-06 20:16:01 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  20:16:01 up  9:32,  1 user,  load average: 0.36, 0.34, 0.32 c67c2988eaf0147a92858f178300c225953c53bf

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -64,7 +64,6 @@ void L1_flush () {
 }
 
 uint32_t L1_read (hwaddr_t addr, size_t len) {
-    printf ("%x %zx\n", addr, len);
     return dram_read (addr, len) & (~0u >> ((4 - len) << 3));
     L1_addr temp;
     temp.addr = addr;
