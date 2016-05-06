af5c7eb9397ca1f34a16dfb9b9694ca2d3e05424
Wu Yuncong <14307130244@fudan.edu.cn>
2016-05-06 19:53:36 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  19:53:36 up  9:09,  1 user,  load average: 0.21, 0.26, 0.34 c99e70d7b958c24ade575f38f3108addacc1565

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -65,6 +65,7 @@ uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
     if ((addr + len - 1) > (addr | 0xfff)) {
         /* this is a special case, you can handle it later. */
         uint32_t result = 0;
+        puts ("hahaha");
         for (int i = 0; i < len; ++i, result <<= 8) result |= lnaddr_read (addr + i, 1);
         return result;
     }
@@ -78,6 +79,7 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
     assert(len == 1 || len == 2 || len == 4);
     if ((addr + len - 1) > (addr | 0xfff)) {
         /* this is a special case, you can handle it later. */
+        puts ("heiheihei");
         for (int i = 0; i < len; ++i, data >>= 8) lnaddr_write (addr + i, 1, data & 0xff);
         return ;
     }
