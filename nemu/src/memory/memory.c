6ca940ac95141dad14a27b9039bca54c4b59cd49
Wu Yuncong <14307130244@fudan.edu.cn>
2016-05-06 20:13:55 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  20:13:55 up  9:30,  1 user,  load average: 0.26, 0.31, 0.30 dea21d4b54e66394e73a33f38ec0e95611e6c046

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -46,7 +46,8 @@ void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
     int tmp = is_mmio (addr);
     if (!~tmp) {
 #ifdef CACHE 
-    L1_write (addr, len, data);
+    dram_write(addr, len, data);
+    //L1_write (addr, len, data);
 #else
     dram_write(addr, len, data);
 #endif
