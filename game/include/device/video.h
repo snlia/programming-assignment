ec53084e7fe7b181fe34431f322dc205e0700dd3
Wu Yuncong <14307130244@fudan.edu.cn>
2016-05-08 21:53:29 +0800

    >  compile game 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  21:53:29 up 11:47,  1 user,  load average: 1.16, 0.56, 0.48 dda4470e9599149994bc8e8ed5108d1e5f52c681

--- game/include/device/video.h
+++ game/include/device/video.h
@@ -13,7 +13,7 @@ extern uint8_t *vmem;
 static inline void
 draw_pixel(int x, int y, int color) {
 	assert(x >= 0 && y >= 0 && x < SCR_HEIGHT && y < SCR_WIDTH);
-    set_bp ();
+    Log ("%d %d %zu\n", x, y, color);
 	vmem[(x << 8) + (x << 6) + y] = color;
 }
 
