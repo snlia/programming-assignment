e63717fc2825c5770cb7a41943b2a92e42fc2e1b
Wu Yuncong <14307130244@fudan.edu.cn>
2016-05-08 22:26:30 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  22:26:30 up 12:20,  1 user,  load average: 0.26, 0.34, 0.44 9023af45a08a431d86869bd540ad43e5ba9e9953

--- nemu/src/cpu/exec/data-mov/bt-template.h
+++ nemu/src/cpu/exec/data-mov/bt-template.h
@@ -3,7 +3,10 @@
 #define instr bt
 
 static void do_execute () {
-    cpu.CF = ((DATA_TYPE)op_dest->val >> (op_src->val & ((DATA_BYTE << 3) - 1))) & 1;
+	DATA_TYPE src = op_src->val;
+	DATA_TYPE dest = op_dest->val;
+    DATA_TYPE off = src % (DATA_BYTE << 3);
+    cpu.CF = (dest >> off) & 1;
 	print_asm_template2();
 }
 
