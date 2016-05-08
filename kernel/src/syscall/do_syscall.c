04ad49c0b3dae922c1e7197c24168312e4130260
Wu Yuncong <14307130244@fudan.edu.cn>
2016-05-08 14:43:14 +0800

    >  compile kernel 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  14:43:14 up  4:36,  1 user,  load average: 0.31, 0.40, 0.62 dbbc8f8c910232876dcb96e0c9f9c02eec1ef6b4

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -16,32 +16,32 @@ static void sys_brk(TrapFrame *tf) {
 static void sys_write (TrapFrame *tf) {
     assert (tf->ebx == 1 || tf->ebx == 2);
     uint32_t addr = tf->ecx;
-    for (int i = 0; i < tf->ecx; ++i)
+    for (int i = 0; i < tf->edx; ++i)
         serial_printc(*((uint8_t*) (addr + i)));
-//    asm volatile (".byte 0xd6" : : "a"(2), "c"(tf->ecx), "d"(tf->edx));
+    //    asm volatile (".byte 0xd6" : : "a"(2), "c"(tf->ecx), "d"(tf->edx));
     tf->eax = tf->edx;
 }
 
 void do_syscall(TrapFrame *tf) {
-	switch(tf->eax) {
-		/* The ``add_irq_handle'' system call is artificial. We use it to 
-		 * let user program register its interrupt handlers. But this is 
-		 * very dangerous in a real operating system. Therefore such a 
-		 * system call never exists in GNU/Linux.
-		 */
-		case 0: 
-			cli();
-			add_irq_handle(tf->ebx, (void*)tf->ecx);
-			sti();
-			break;
+    switch(tf->eax) {
+        /* The ``add_irq_handle'' system call is artificial. We use it to 
+         * let user program register its interrupt handlers. But this is 
+         * very dangerous in a real operating system. Therefore such a 
+         * system call never exists in GNU/Linux.
+         */
+        case 0: 
+            cli();
+            add_irq_handle(tf->ebx, (void*)tf->ecx);
+            sti();
+            break;
 
-		case SYS_brk: sys_brk(tf); break;
+        case SYS_brk: sys_brk(tf); break;
 
         case SYS_write: sys_write(tf); break;
 
-		/* TODO: Add more system calls. */
+                        /* TODO: Add more system calls. */
 
-		default: panic("Unhandled system call: id = %d", tf->eax);
-	}
+        default: panic("Unhandled system call: id = %d", tf->eax);
+    }
 }
 
