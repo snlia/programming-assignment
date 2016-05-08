a0f09d34f0747860ea8231d56fce57fc48689798
Wu Yuncong <14307130244@fudan.edu.cn>
2016-05-08 12:23:09 +0800

    >  compile kernel 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  12:23:09 up  2:16,  1 user,  load average: 1.72, 1.23, 1.01 3be70e812bacacbaf354f32bd984cdabc941fda0

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -14,41 +14,54 @@ void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
-	 * some page tables to create this mapping.
-	 */
+     * some page tables to create this mapping.
+     */
     PDE *updir = get_updir();
-	PTE *ptable = (PTE *)va_to_pa(vptable);
+    PTE *ptable = (PTE *)va_to_pa(vptable);
+    updir[0].val = make_pde(ptable);
+
+    memset(vptable, 0, sizeof(vptable));
+
+    uint32_t vmem_addr = VMEM_ADDR;
+    uint32_t idx = VMEM_ADDR / PAGE_SIZE;
+    for (vmem_addr = VMEM_ADDR; vmem_addr < VMEM_ADDR + VMEM_SIZE; vmem_addr += PAGE_SIZE) {
+        vptable[idx].val = make_pte(vmem_addr);
+        idx++;
+    }
+    /*
+    PDE *updir = get_updir();
+    PTE *ptable = (PTE *)va_to_pa(vptable);
     updir[0].val = make_pde(ptable);
 
     memset (ptable, 0, sizeof (ptable));
 
-	asm volatile ("std;\
-	 1: stosl;\
-		subl %0, %%eax;\
-        cmpl $0xa0000, %%eax;\
-		jge 1b" : : 
-		"i"(PAGE_SIZE), "a"((VMEM_ADDR + VMEM_SIZE - PAGE_SIZE) | 0x7), "D"(ptable + (VMEM_ADDR + VMEM_SIZE - PAGE_SIZE) / PAGE_SIZE));
-
-//	panic("please implement me");
+    asm volatile ("std;\
+            1: stosl;\
+            subl %0, %%eax;\
+            cmpl $0xa0000, %%eax;\
+            jge 1b" : : 
+            "i"(PAGE_SIZE), "a"((VMEM_ADDR + VMEM_SIZE - PAGE_SIZE) | 0x7), "D"(ptable + (VMEM_ADDR + VMEM_SIZE - PAGE_SIZE) / PAGE_SIZE));
+*/
+    //	panic("please implement me");
 }
 
 void video_mapping_write_test() {
-	int i;
-	uint32_t *buf = (void *)VMEM_ADDR;
-	for(i = 0; i < SCR_SIZE / 4; i ++) {
-		buf[i] = i;
-	}
+    int i;
+    uint32_t *buf = (void *)VMEM_ADDR;
+    for(i = 0; i < SCR_SIZE / 4; i ++) {
+        buf[i] = i;
+    }
 }
 
 void video_mapping_read_test() {
-	int i;
-	uint32_t *buf = (void *)VMEM_ADDR;
-	for(i = 0; i < SCR_SIZE / 4; i ++) {
-		assert(buf[i] == i);
-	}
+    int i;
+    uint32_t *buf = (void *)VMEM_ADDR;
+    for(i = 0; i < SCR_SIZE / 4; i ++) {
+        assert(buf[i] == i);
+    }
 }
 
 void video_mapping_clear() {
-	memset((void *)VMEM_ADDR, 0, SCR_SIZE);
+    memset((void *)VMEM_ADDR, 0, SCR_SIZE);
 }
 
