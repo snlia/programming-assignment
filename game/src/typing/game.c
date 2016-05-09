3861026e7e6b745239de44f2a534465ca02e1046
Wu Yuncong <14307130244@fudan.edu.cn>
2016-05-09 10:27:15 +0800

    >  compile game 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  10:27:15 up  1:57,  1 user,  load average: 0.16, 0.18, 0.29 e45048241b789e2fb3e8f099f7ea0efc37b91f4b

--- game/src/typing/game.c
+++ game/src/typing/game.c
@@ -40,7 +40,6 @@ main_loop(void) {
 	bool redraw;
 
 	while (true) {
-        set_bp ();
 		wait_intr();
 		cli();
 		if (now == tick) {
@@ -87,6 +86,7 @@ main_loop(void) {
 		if (redraw) { /* 当需要重新绘图时重绘 */
 			num_draw ++;
 			redraw_screen();
+            Log ("%d\n", num_draw);
 		}
 	}
 }
