aa9de9c6c165537782acd0f3fc08936367d5a33f
Wu Yuncong <14307130244@fudan.edu.cn>
2016-05-09 12:50:00 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  12:50:00 up  4:20,  1 user,  load average: 0.67, 0.51, 0.51 ae228b133b701801707612baf290caa9a18a8f2e

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -2,8 +2,6 @@ void init_monitor(int, char *[]);
 void reg_test();
 void restart();
 void ui_mainloop();
-extern void init_device ();
-extern void init_sdl ();
 
 int main(int argc, char *argv[]) {
 	/* Initialize the monitor. */
@@ -16,8 +14,6 @@ int main(int argc, char *argv[]) {
 	restart();
 
 	/* Receive commands from user. */
-    init_device();
-    init_sdl();
 	ui_mainloop();
 
 	return 0;
--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -11,85 +11,91 @@ void load_elf_tables(int, char *[]);
 void init_regex();
 void init_wp_list();
 void init_ddr3();
+extern void init_device ();
+extern void init_sdl ();
 
 FILE *log_fp = NULL;
 
 static void init_log() {
-	log_fp = fopen("log.txt", "w");
-	Assert(log_fp, "Can not open 'log.txt'");
+    log_fp = fopen("log.txt", "w");
+    Assert(log_fp, "Can not open 'log.txt'");
 }
 
 static void welcome() {
-	printf("Welcome to NEMU!\nThe executable is %s.\nFor help, type \"help\"\n",
-			exec_file);
+    printf("Welcome to NEMU!\nThe executable is %s.\nFor help, type \"help\"\n",
+            exec_file);
 }
 
 void init_monitor(int argc, char *argv[]) {
-	/* Perform some global initialization */
+    /* Perform some global initialization */
 
-	/* Open the log file. */
-	init_log();
+    /* Open the log file. */
+    init_log();
 
-	/* Load the string table and symbol table from the ELF file for future use. */
-	load_elf_tables(argc, argv);
+    /* Load the string table and symbol table from the ELF file for future use. */
+    load_elf_tables(argc, argv);
 
-	/* Compile the regular expressions. */
-	init_regex();
+    /* Compile the regular expressions. */
+    init_regex();
 
-	/* Initialize the watchpoint link list. */
-	init_wp_list();
+    /* Initialize the watchpoint link list. */
+    init_wp_list();
 
-	/* Display welcome message. */
-	welcome();
+    /* Display welcome message. */
+    welcome();
+#ifdef HAS_DEVICE
+    init_device();
+    init_sdl();
+#endif
 }
 
 #ifdef USE_RAMDISK
 static void init_ramdisk() {
-	int ret;
-	const int ramdisk_max_size = 0xa0000;
-	FILE *fp = fopen(exec_file, "rb");
-	Assert(fp, "Can not open '%s'", exec_file);
+    int ret;
+    const int ramdisk_max_size = 0xa0000;
+    FILE *fp = fopen(exec_file, "rb");
+    Assert(fp, "Can not open '%s'", exec_file);
 
-	fseek(fp, 0L, SEEK_END);
-	size_t file_size = ftell(fp);
-	Assert(file_size < ramdisk_max_size, "file size(%zd) too large", file_size);
+    fseek(fp, 0L, SEEK_END);
+    size_t file_size = ftell(fp);
+    Assert(file_size < ramdisk_max_size, "file size(%zd) too large", file_size);
 
-	fseek(fp, 0L, SEEK_SET);
-	ret = fread(hwa_to_va(0), file_size, 1, fp);
-	assert(ret == 1);
-	fclose(fp);
+    fseek(fp, 0L, SEEK_SET);
+    ret = fread(hwa_to_va(0), file_size, 1, fp);
+    assert(ret == 1);
+    fclose(fp);
 }
 #endif
 
 static void load_entry() {
-	int ret;
-	FILE *fp = fopen("entry", "rb");
-	Assert(fp, "Can not open 'entry'");
+    int ret;
+    FILE *fp = fopen("entry", "rb");
+    Assert(fp, "Can not open 'entry'");
 
-	fseek(fp, 0L, SEEK_END);
-	size_t file_size = ftell(fp);
+    fseek(fp, 0L, SEEK_END);
+    size_t file_size = ftell(fp);
 
-	fseek(fp, 0L, SEEK_SET);
-	ret = fread(hwa_to_va(ENTRY_START), file_size, 1, fp);
-	assert(ret == 1);
-	fclose(fp);
+    fseek(fp, 0L, SEEK_SET);
+    ret = fread(hwa_to_va(ENTRY_START), file_size, 1, fp);
+    assert(ret == 1);
+    fclose(fp);
 }
 
 void restart() {
-	/* Perform some initialization to restart a program */
+    /* Perform some initialization to restart a program */
 #ifdef USE_RAMDISK
-	/* Read the file with name `argv[1]' into ramdisk. */
-	init_ramdisk();
+    /* Read the file with name `argv[1]' into ramdisk. */
+    init_ramdisk();
 #endif
 
-	/* Read the entry code into memory. */
-	load_entry();
+    /* Read the entry code into memory. */
+    load_entry();
 
-	/* Set the initial instruction pointer. */
-	cpu.eip = ENTRY_START;
-	cpu.eflags = EFLAGS_INIT;
+    /* Set the initial instruction pointer. */
+    cpu.eip = ENTRY_START;
+    cpu.eflags = EFLAGS_INIT;
     cache_SEG[1].val = 0xcf92000000ffff;
 
-	/* Initialize DRAM. */
-	init_ddr3();
+    /* Initialize DRAM. */
+    init_ddr3();
 }
