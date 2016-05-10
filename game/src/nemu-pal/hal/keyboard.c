6b924525b092a829e0576ad5ffae9fab7b69aa9d
Wu Yuncong <14307130244@fudan.edu.cn>
2016-05-10 23:18:06 +0800

    >  compile game 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  23:18:06 up 15:26,  1 user,  load average: 0.45, 0.81, 0.88 2c8bbc48ed6f0f667854b41fa9dac71ada4e32e

--- game/src/nemu-pal/hal/keyboard.c
+++ game/src/nemu-pal/hal/keyboard.c
@@ -1,56 +1,85 @@
 #include "hal.h"
-#include "x86.h"
 
 #define NR_KEYS 18
-#define I8042_DATA_PORT 0x60
 
-enum {KEY_STATE_RELEASED, KEY_STATE_PRESSED, KEY_STATE_PENDING_RELEASE, KEY_STATE_PRESS};
+enum {KEY_STATE_EMPTY, KEY_STATE_WAIT_RELEASE, KEY_STATE_RELEASE, KEY_STATE_PRESS};
 
 /* Only the following keys are used in NEMU-PAL. */
 static const int keycode_array[] = {
-    K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE,
-    K_RETURN, K_SPACE, K_PAGEUP, K_PAGEDOWN, K_r,
-    K_a, K_d, K_e, K_w, K_q,
-    K_s, K_f, K_p
+	K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE,
+	K_RETURN, K_SPACE, K_PAGEUP, K_PAGEDOWN, K_r,
+	K_a, K_d, K_e, K_w, K_q,
+	K_s, K_f, K_p
 };
 
 static int key_state[NR_KEYS];
 
+static int key_code = 0;
+
 void
 keyboard_event(void) {
-    uint8_t scancode = in_byte(I8042_DATA_PORT);
-    bool release = !!(scancode & 0x80);
-    scancode = scancode & ~(0x80);
-    int index = -1;
-    for (size_t i = 0; i < NR_KEYS; i++) {
-        if (scancode == keycode_array[i]) {
-            index = i;
-            break;
-        }
-    }
-    if (index == -1) return;
-    if (release) return;
-    if (key_state[index] == KEY_STATE_RELEASED) {
-        key_state[index] = KEY_STATE_PRESS;
-    } else if (key_state[index] == KEY_STATE_PENDING_RELEASE) {
-        key_state[index] = KEY_STATE_PRESSED;
-    }
+	/* TODO: Fetch the scancode and update the key states. */
+	key_code = in_byte(0x60);
+    bool release = ((key_code & 0x80) >> 7) & 1;
+    key_code &= ~0x80;
+    int i = 0;
+    for ( ; i < NR_KEYS; ++i)
+        if (key_code == keycode_array[i]) break;
+    if (i == NR_KEYS) return ;
+    if (!release && key_state[i] == KEY_STATE_EMPTY) key_state[i] = KEY_STATE_PRESS;
+    if (release && key_state[i] == KEY_STATE_WAIT_RELEASE) key_state[i] = KEY_STATE_RELEASE;
+	//assert(0);
+}
+
+static inline int
+get_keycode(int index) {
+	assert(index >= 0 && index < NR_KEYS);
+	return keycode_array[index];
+}
+
+static inline int
+query_key(int index) {
+	assert(index >= 0 && index < NR_KEYS);
+	return key_state[index];
+}
+
+static inline void
+release_key(int index) {
+	assert(index >= 0 && index < NR_KEYS);
+	key_state[index] = KEY_STATE_WAIT_RELEASE;
+}
+
+static inline void
+clear_key(int index) {
+	assert(index >= 0 && index < NR_KEYS);
+	key_state[index] = KEY_STATE_EMPTY;
 }
 
 bool 
 process_keys(void (*key_press_callback)(int), void (*key_release_callback)(int)) {
-    cli();
-    for (int index = 0; index < NR_KEYS; index++) {
-        if (key_state[index] == KEY_STATE_PRESS) {
-            key_press_callback(keycode_array[index]);
-            key_state[index] = KEY_STATE_PENDING_RELEASE;
-        } else if (key_state[index] == KEY_STATE_PENDING_RELEASE) {
-            key_release_callback(keycode_array[index]);
-            key_state[index] = KEY_STATE_RELEASED;
-        } else if (key_state[index] == KEY_STATE_PRESSED) {
-            key_state[index] = KEY_STATE_PENDING_RELEASE;
+	cli();
+	/* TODO: Traverse the key states. Find a key just pressed or released.
+	 * If a pressed key is found, call ``key_press_callback'' with the keycode.
+	 * If a released key is found, call ``key_release_callback'' with the keycode.
+	 * If any such key is found, the function return true.
+	 * If no such key is found, the function return false.
+	 * Remember to enable interrupts before returning from the function.
+	 */
+    for (int i = 0; i < NR_KEYS; ++i)  {
+        if (key_state[i] == KEY_STATE_PRESS) {
+            key_press_callback (keycode_array[i]);
+            key_state[i] = KEY_STATE_WAIT_RELEASE;
+            sti();
+            return true;
+        }
+        if (key_state[i] == KEY_STATE_RELEASE) {
+            key_release_callback (keycode_array[i]);
+            key_state[i] = KEY_STATE_EMPTY;
+            sti();
+            return true;
         }
     }
+
     sti();
     return false;
 }
