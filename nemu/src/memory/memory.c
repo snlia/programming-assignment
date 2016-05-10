984872b4269668125acf4b470c4741d9a32586b1
Wu Yuncong <14307130244@fudan.edu.cn>
2016-05-10 11:38:05 +0800

    >  compile NEMU 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  11:38:05 up  3:46,  1 user,  load average: 0.24, 0.36, 0.36 4882ba33e870212a1b7cbf552c713fe8febfa8c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -98,9 +98,7 @@ lnaddr_t seg_translate (swaddr_t swaddr, uint8_t sreg) {
 }
 
 uint32_t swaddr_read(swaddr_t addr, size_t len, uint8_t sreg) {
-#ifdef DEBUG
     assert(len == 1 || len == 2 || len == 4);
-#endif
 #ifndef O1 
     addr = seg_translate(addr, sreg);
 #endif
@@ -108,9 +106,7 @@ uint32_t swaddr_read(swaddr_t addr, size_t len, uint8_t sreg) {
 }
 
 void swaddr_write(swaddr_t addr, size_t len, uint32_t data, size_t sreg) {
-#ifdef DEBUG
     assert(len == 1 || len == 2 || len == 4);
-#endif
 #ifndef O1
     addr = seg_translate(addr, sreg);
 #endif
