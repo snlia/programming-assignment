250104a69fcec2f3cccefb31ae92e9bbf3808e3e
Wu Yuncong <14307130244@fudan.edu.cn>
2016-05-10 11:20:14 +0800

    >  compile game 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  11:20:14 up  3:28,  1 user,  load average: 0.16, 0.27, 0.29 d7707ae4d43b1d0a74baed516a1be674d546d2f5

--- game/src/nemu-pal/main.c
+++ game/src/nemu-pal/main.c
@@ -149,7 +149,6 @@ PAL_Init(
    Log("PAL_InitResources success");
    SOUND_OpenAudio();
 
-   Log ("get here");
 #ifdef PAL_WIN95
 #ifdef _DEBUG
    SDL_WM_SetCaption("Pal WIN95 (Debug Build)", NULL);
@@ -163,7 +162,6 @@ PAL_Init(
    SDL_WM_SetCaption("Pal", NULL);
 #endif
 #endif
-   Log ("get here");
 }
 
 VOID
