1f4e24d7d0ccc9e62f795b79908cbf9f36c65336
Wu Yuncong <14307130244@fudan.edu.cn>
2016-05-10 10:16:16 +0800

    >  compile kernel 14307130244 snlia Linux SnliaPC 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  10:16:16 up  2:24,  1 user,  load average: 0.14, 0.12, 0.13 eaab4a44a3122df33cd12975e984f7025232dcad

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -2,48 +2,55 @@
 
 #include <sys/syscall.h>
 
+int fs_open(const char *, int );   /* 在我们的实现中可以忽略flags */
+int fs_read(int , void *, int );
+int fs_write(int , void *, int );
+int fs_lseek(int , int , int );
+int fs_close(int );
+
 void add_irq_handle(int, void (*)(void));
 void mm_brk(uint32_t);
 extern void serial_printc (char);
 
 static void sys_brk(TrapFrame *tf) {
 #ifdef IA32_PAGE
-	mm_brk(tf->ebx);
+    mm_brk(tf->ebx);
 #endif
-	tf->eax = 0;
+    tf->eax = 0;
 }
 
 uint32_t edi, esi, ebp, old_esp, ebx, edx, ecx, eax; //push to stack by pusha
 
 static void sys_write (TrapFrame *tf) {
     if (tf->ebx == 1 || tf->ebx == 2) {
+        //    asm volatile (".byte 0xd6" : : "a"(2), "c"(tf->ecx), "d"(tf->edx));
         uint8_t *addr = (uint8_t *)tf->ecx;
         for (int i = 0; i < tf->edx; ++i)
             serial_printc(*(addr + i));
         tf->eax = tf->edx;
     }
-    else {
-        Log ("%d %d %d %d\n", tf->ebx, tf->ecx, tf->edx);
-        assert (0);
-    }
-    //    asm volatile (".byte 0xd6" : : "a"(2), "c"(tf->ecx), "d"(tf->edx));
+    else 
+        tf->eax = fs_write (tf->ebx, (void *) tf->ecx, tf->edx);
 }
 
 static void sys_read (TrapFrame *tf) {
     if (!tf->ebx) {
         panic ("try to read from stdin");
     }
-    else {
-        Log ("%d %d %d %d\n", tf->eax, tf->ebx, tf->ecx, tf->edx);
-        assert (0);
-    }
-    //    asm volatile (".byte 0xd6" : : "a"(2), "c"(tf->ecx), "d"(tf->edx));
+    else 
+        tf->eax = fs_read (tf->ebx, (void *) tf->ecx, tf->edx);
 }
 
 static void sys_open (TrapFrame *tf) {
-    Log ("%d %d %d %d\n", tf->eax, tf->ebx, tf->ecx, tf->edx);
-    assert (0);
-    //    asm volatile (".byte 0xd6" : : "a"(2), "c"(tf->ecx), "d"(tf->edx));
+    tf->eax = fs_open ((char*)tf->ebx, tf->ecx);
+}
+
+static void sys_close (TrapFrame *tf) {
+    tf->eax = fs_close (tf->ebx);
+}
+
+static void sys_lseek (TrapFrame *tf) {
+    tf->eax = fs_lseek (tf->ebx, tf->ecx, tf->edx);
 }
 
 void do_syscall(TrapFrame *tf) {
@@ -66,13 +73,12 @@ void do_syscall(TrapFrame *tf) {
         case SYS_read: sys_read(tf); break;
 
         case SYS_open: sys_open(tf); break;
-                       /*
-                          case SYS_lseek: sys_lseek(tf); break;
 
-                          case SYS_close: sys_close(tf); break;
-                          */
+        case SYS_lseek: sys_lseek(tf); break;
 
-                       /* TODO: Add more system calls. */
+        case SYS_close: sys_close(tf); break;
+
+                        /* TODO: Add more system calls. */
 
         default: panic("Unhandled system call: id = %d", tf->eax);
     }
